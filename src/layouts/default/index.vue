98386b7a2d84c2aeb4765e3b024e8dd6172fb6cf
anhtv56 <anhtv56@ghtk.co>
2022-10-04 15:07:48 +0700

    update

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -1,17 +1,5 @@
-<script lang="ts" setup>
-import { MenuRight } from '@/layout-components';
-</script>
-
 <template>
-  <div id="__main" class="flex w-full">
-    <div id="__content">
-      <router-view />
-    </div>
-
-    <div id="__menu" class="w-80">
-      <MenuRight />
-    </div>
+  <div id="__main">
+    <router-view />
   </div>
 </template>
-
-<style scoped lang="scss" src="./styles.scss"></style>
