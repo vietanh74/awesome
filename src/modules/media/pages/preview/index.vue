70bcd4f37c000e0a75e056e56a4355e25f0aa94b
vietanh <vietanh>
2026-07-11 11:38:51 +0700

    feat: update

--- src/modules/media/pages/preview/index.vue
+++ src/modules/media/pages/preview/index.vue
@@ -19,7 +19,7 @@
             <div
               class="px-3 py-2.5 flex items-start justify-between gap-3 bg-slate-50/50 border-b border-slate-100"
             >
-              <div class="font-medium text-slate-700 text-sm break-all leading-tight mt-0.5">
+              <div class="font-medium text-slate-700 text-sm break-words leading-tight mt-0.5 flex-1 min-w-0">
                 {{ item.name }}
               </div>
               <Dropdown :trigger="['click']" placement="bottomRight">
